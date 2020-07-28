a77a8eb3b28d486e84048498b014e6570409e08f
kuikahi-vance <vancekuikahi@gmail.com>
2020-07-28 00:27:32 -0700

    created base game

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    public GameObject player;
+    private Vector3 offset;
+
+    void Start() {
+        // difference between player GameObject and the camera
+        // camera position - player position
+        offset = transform.position - player.transform.position;
+    }
+
+    // LateUpdate will run every frame, but after all Update() functions in game are done
+    // Ensures this does not run before another Update() function
+    void LateUpdate() {
+        // using solely the position of the player, not directional information
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿// Checks every frame for movement
+// Apply movement to player object (Update() or FixedUpdate())
+// FixedUpdate: applied before any physics update
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;  // functions of InputSystem
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 0;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+
+    void Start() {
+        rb = GetComponent<Rigidbody>(); // sets rb to Rigidbody reference in GameObject
+    }
+
+    void OnMove(InputValue movementValue) {
+        Vector2 movementVector = movementValue.Get<Vector2>();  // gets Vector2 data from movementValue
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    // adds force to player's Rigidbody
+    void FixedUpdate() {
+        Vector3 movement = new Vector3(movementX, 0.0f, movementY);
+        rb.AddForce(movement * speed);
+    }
+
+}
