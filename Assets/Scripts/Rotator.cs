12cfeb162d629669724bfb481582be71e642a6c4
kuikahi-vance <vancekuikahi@gmail.com>
2020-07-28 00:52:13 -0700

    finished rotation script

--- Assets/Scripts/Rotator.cs
+++ Assets/Scripts/Rotator.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+public class Rotator : MonoBehaviour {
+    
+    // Before rendering each frame...
+    void Update() {
+        // Rotate the game object that this script is attached to by 15 in X axis,
+        // 30 in Y axis, and 45 in Z axis
+        // Multiply by timeDelta to rotate per second, rather than per frame.
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime); // deltaTime: difference in seconds when the last frame occured
     }
 }
